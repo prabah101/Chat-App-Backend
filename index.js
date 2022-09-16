c4039123376f810dcaad080e60daf71febd0ba6d
GamBit101 <ppkalita009@gmail.com>
2022-09-16 15:16:27 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: true,
         credentials: true
     }
 })
