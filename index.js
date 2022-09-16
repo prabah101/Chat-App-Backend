09e7529f7b73339785d1d5cebec829d2e1c439a6
Prabah Prasad Kalita <76781633+prabah101@users.noreply.github.com>
2022-09-16 15:51:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: true,
+        origin: "https://converse-chat-app-cheery-lollipop-23.netlify.app",
         credentials: true
     }
 })
